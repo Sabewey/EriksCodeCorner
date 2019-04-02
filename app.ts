4a3a761f62f016271971dced72596cb7ccfc0d61
Mark <38922368+Mantaur@users.noreply.github.com>
2019-04-02 14:15:54 +0200

    Paddle Tennis game typescript file

---
+++ app.ts
@@ -0,0 +1,21 @@
+class Paddle {
+
+}
+
+class Ball {
+
+}
+
+class Table {
+    
+}
+
+class App {
+    public start() {
+        console.log("Starting application...")
+    }
+}
+
+const app = new App();
+app.start();
+
